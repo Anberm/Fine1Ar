77840d3375d16a936d7fc6b5e5eb529f09dd80fc
Anberm <shuangyan_m@hotmail.com>
2020-03-09 15:28:38 +0800

    回到原点敏感度增强

--- Fine1ArAndroid/app/src/main/java/com/fine1/ar/MotionDetector.java
+++ Fine1ArAndroid/app/src/main/java/com/fine1/ar/MotionDetector.java
@@ -25,6 +25,9 @@ class MotionDetector implements SensorEventListener {
 
     private boolean mIsEnable;
 
+    private boolean oIsEnable=true;
+    private static final int O_DETECTION_INTERVAL = 1000;     // 1000 ms
+
     private SensorManager mSensorManager;
     private boolean mIsStartSensor;
     private IMotionDetectListener mListener;
@@ -32,9 +35,9 @@ class MotionDetector implements SensorEventListener {
     private final Handler mHandler;
 
     // 速度阈值，当摇晃速度达到这值后产生作用
-    private static final int SPEED_SHRESHOLD = 700;
+    private static final int SPEED_SHRESHOLD = 200;
     // 两次检测的时间间隔
-    private static final int UPTATE_INTERVAL_TIME = 50;
+    private static final int UPTATE_INTERVAL_TIME = 100;
     private float lastX;
     private float lastY;
     private float lastZ;
@@ -66,42 +69,47 @@ class MotionDetector implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
             if (mListener != null) {
-                if (mIsEnable) {
-                    int direction = judgeDirection(event.values);
-                    mListener.onMotion(direction);
-                    mIsEnable = false;
-                    startIntervalTimer();
-                }
-                mListener.onChanged(event.values[0], event.values[1], event.values[2]);
-                // 现在检测时间
-                long currentUpdateTime = System.currentTimeMillis();
-                // 两次检测的时间间隔
-                long timeInterval = currentUpdateTime - lastUpdateTime;
-                // 判断是否达到了检测时间间隔
-                if (timeInterval < UPTATE_INTERVAL_TIME) return;
-                // 现在的时间变成last时间
-                lastUpdateTime = currentUpdateTime;
-                // 获得x,y,z坐标
-                float x = event.values[0];
-                float y = event.values[1];
-                float z = event.values[2];
-                // 获得x,y,z的变化值
-                float deltaX = x - lastX;
-                float deltaY = y - lastY;
-                float deltaZ = z - lastZ;
-                // 将现在的坐标变成last坐标
-                lastX = x;
-                lastY = y;
-                lastZ = z;
-                double speed = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ
-                        * deltaZ)
-                        / timeInterval * 10000;
-                // 达到速度阀值，发出提示
-
-                if (speed >= SPEED_SHRESHOLD) {
+                if(oIsEnable){
+                    if (mIsEnable) {
+                        int direction = judgeDirection(event.values);
+                        mListener.onMotion(direction);
+                        mIsEnable = false;
+                        startIntervalTimer();
+                    }
+                    mListener.onChanged(event.values[0], event.values[1], event.values[2]);
+                    // 现在检测时间
+                    long currentUpdateTime = System.currentTimeMillis();
+                    // 两次检测的时间间隔
+                    long timeInterval = currentUpdateTime - lastUpdateTime;
+                    // 判断是否达到了检测时间间隔
+                    if (timeInterval < UPTATE_INTERVAL_TIME) return;
+                    // 现在的时间变成last时间
+                    lastUpdateTime = currentUpdateTime;
+                    // 获得x,y,z坐标
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+                    // 获得x,y,z的变化值
+                    float deltaX = x - lastX;
+                    float deltaY = y - lastY;
+                    float deltaZ = z - lastZ;
+                    // 将现在的坐标变成last坐标
+                    lastX = x;
+                    lastY = y;
+                    lastZ = z;
+                    double speed = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ
+                            * deltaZ)
+                            / timeInterval * 10000;
+                    // 达到速度阀值，发出提示
                     Log.d(TAG,"速度："+speed);
-                    mListener.onShake();
+                    if (speed >= SPEED_SHRESHOLD) {
+                        oIsEnable=false;
+                        Log.d(TAG,"速度 Shake："+speed);
+                        mListener.onShake();
+                        startOIntervalTimer();
+                    }
                 }
+
 
             }
         }
@@ -158,10 +166,26 @@ class MotionDetector implements SensorEventListener {
         }
     }
 
+    private void startOIntervalTimer() {
+        if (mHandler != null) {
+            mHandler.postDelayed(oTimeout, O_DETECTION_INTERVAL);
+        }
+    }
+
+
+
+
     private final Runnable mTimeout = new Runnable() {
         @Override
         public void run() {
             mIsEnable = true;
+        }
+    };
+
+    private final Runnable oTimeout = new Runnable() {
+        @Override
+        public void run() {
+            oIsEnable = true;
         }
     };
 }
